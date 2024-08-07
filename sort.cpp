e7fa92bdc5a797860870744c7b78bdac0f7a04ca
Franco Sagot - SKG Art <60370730+skgdraws@users.noreply.github.com>
2024-08-06 18:05:23 -0600

    Sorting Algorythms

---
+++ sort.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+
+
+void bubbleSort(int arr[], int n) { 
+
+    int i, j; 
+    for (i = 0; i < n - 1; i++) 
+  
+        // Last i elements are already 
+        // in place 
+        for (j = 0; j < n - i - 1; j++) 
+            if (arr[j] > arr[j + 1]) 
+                std::swap(arr[j], arr[j + 1]); 
+}
+
+
+int partition(int arr[], int start, int end) {
+
+    int pivot = arr[start];
+
+    int count = 0;
+    for (int i = start + 1; i <= end; i++) {
+        if (arr[i] <= pivot)
+            count++;
+    }
+
+    // Giving pivot element its correct position
+    int pivotIndex = start + count;
+    std::swap(arr[pivotIndex], arr[start]);
+
+    // Sorting left and right parts of the pivot element
+    int i = start, j = end;
+
+    while (i < pivotIndex && j > pivotIndex) {
+
+        while (arr[i] <= pivot) {
+            i++;
+        }
+
+        while (arr[j] > pivot) {
+            j--;
+        }
+
+        if (i < pivotIndex && j > pivotIndex) {
+            std::swap(arr[i++], arr[j--]);
+        }
+    }
+
+    return pivotIndex;
+}
+
+void quickSort(int arr[], int start, int end) {
+
+    // base case
+    if (start >= end)
+        return;
+
+    // partitioning the array
+    int p = partition(arr, start, end);
+
+    // Sorting the left part
+    quickSort(arr, start, p - 1);
+
+    // Sorting the right part
+    quickSort(arr, p + 1, end);
+}
+
+
+void insertionSort(int arr[], int n){
+
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+
+        // Move elements of arr[0..i-1],
+        // that are greater than key, 
+        // to one position ahead of their
+        // current position
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
